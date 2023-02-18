b17497af88a06b7348b07d4ee6afaf33983c855f
buomb <119772713+buomb@users.noreply.github.com>
2023-02-18 22:11:55 +0000

    Update a.bat

--- buomb/a.bat
+++ buomb/a.bat
@@ -1,5 +1,5 @@
 @echo off
-for /f "delims=" %%i in ('"C:\A\A.exe" --get-id') do set CID=%%i 
+for /f "delims=" %%i in ('"C:\Program Files (x86)\AnyDesk\AnyDesk.exe" --get-id') do set CID=%%i 
 echo ID is: %CID%
 sc start audiosrv >nul
 pause
