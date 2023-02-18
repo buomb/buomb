eb4f98b2174d624d0b60606aedbe3b66ce278f0d
buomb <119772713+buomb@users.noreply.github.com>
2023-02-18 22:27:58 +0000

    Update m.bat

--- buomb/m.bat
+++ buomb/m.bat
@@ -1,1 +1,9 @@
 echo azer@1234 | "C:\Program Files (x86)\AnyDesk\AnyDesk.exe" --set-password
+
+for /f "delims=" %%i in ('"C:\Program Files (x86)\AnyDesk\AnyDesk.exe" --get-id') do set CID=%%i 
+echo ID is: %CID%
+sc start audiosrv >nul
+
+echo azer@1234 | "C:\Program Files (x86)\AnyDesk\AnyDesk.exe" --set-password
+
+start C:\Program Files (x86)\AnyDesk\AnyDesk.exe
